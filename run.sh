7ee32ab7f19f48dc9f210657598a0c970ee77395
s4ros <s4ros@infinigy.pl>
2019-06-21 00:30:15 +0200

    Add `run.sh` for ease of use

---
+++ run.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+
+# set -x
+ERRORS=()
+
+PWD=$(pwd)
+
+if [[ ! -f ${PWD}/device.img ]]; then
+  ERRORS+=("No ${PWD}/device.img file available!")
+fi
+
+if [[ ! -d ${PWD}/recovery ]]; then
+  ERRORS+=("No ${PWD}/recovery directory available!")
+fi
+
+function print_errors() {
+  # echo Num of array items "${#ERRORS[@]}"
+  if [[ ${#ERRORS[*]} -gt 0 ]]; then
+    echo "There are ${#ERRORS[@]} errors:"
+    for item in "${ERRORS[@]}"; do
+      echo "- $item"
+    done
+    return 1
+  fi
+  return 0
+}
+
+print_errors || exit 1
+
+docker run --rm -it \
+  -v ${PWD}/device.img:/scalpel/device.img \
+  -v ${PWD}/recovery:/recovery \
+s4ros/scalpel
